9ca483101a8af7bb7163dcd50119cd42c42587db
Seriy615 <140434578+Seriy615@users.noreply.github.com>
2023-09-10 00:05:54 +0300

    Update 2048-2.py

--- 2048-2.py
+++ 2048-2.py
@@ -40,7 +40,66 @@ def draw_board(board):
                 text = font.render(str(board[i][j]), True, BLACK)
                 text_rect = text.get_rect(center=(j * 100 + 50, i * 100 + 50))
                 screen.blit(text, text_rect)
+def slide_left(board):
+    for row in board:
+        # Сжимаем ряд влево
+        row[:] = [val for val in row if val != 0]
+        row.extend([0] * (4 - len(row)))
 
+def merge_left(board):
+    for row in board:
+        for i in range(3):
+            if row[i] == row[i + 1] and row[i] != 0:
+                row[i] *= 2
+                row[i + 1] = 0
+
+def move_left(board):
+    slide_left(board)
+    merge_left(board)
+    slide_left(board)
+
+def move_right(board):
+    # Зеркально отражаем доску и используем существующий код для движения влево
+    mirror_board(board)
+    move_left(board)
+    mirror_board(board)
+
+def move_up(board):
+    # Транспонируем доску (строки становятся столбцами) и используем код для движения влево
+    transpose_board(board)
+    move_left(board)
+    transpose_board(board)
+
+def move_down(board):
+    # Транспонируем доску и отражаем ее, затем используем код для движения влево
+    transpose_board(board)
+    mirror_board(board)
+    move_left(board)
+    mirror_board(board)
+    transpose_board(board)
+
+def is_game_over(board):
+    # Проверяем условие победы (наличие 2048)
+    for row in board:
+        if 2048 in row:
+            return True
+
+    # Проверяем, есть ли еще ходы (нет пустых ячеек и нет соседних равных чисел)
+    for i in range(4):
+        for j in range(3):
+            if board[i][j] == board[i][j + 1] or board[j][i] == board[j + 1][i]:
+                return False
+
+    return True
+
+def transpose_board(board):
+    # Транспонируем доску (строки становятся столбцами)
+    board[:] = [list(row) for row in zip(*board)]
+
+def mirror_board(board):
+    # Отражаем доску по вертикали
+    for row in board:
+        row.reverse()
 # Основной цикл игры
 running = True
 while running:
